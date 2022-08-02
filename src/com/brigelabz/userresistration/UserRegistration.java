bf44ef5fdd171ac708c47d149083bc4de4aa4edd
tejuypatil <tejuypatil@gmail.com>
2022-08-02 21:04:34 +0530

    Added Program to check Password rule2 by using minimum 8 Characters and 1 Uppercase letter

--- src/com/brigelabz/userresistration/UserRegistration.java
+++ src/com/brigelabz/userresistration/UserRegistration.java
@@ -1,7 +1,30 @@
 package com.brigelabz.userresistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+
+        UserRegistration password = new UserRegistration();
+        System.out.println("Enter Password Minimum 8 characters : ");
+        String userPassword= scanner.nextLine();
+        password.checkPasswordRuleThird(userPassword);
+    }
+
+    public void checkPasswordRuleThird(String userPassword){
+        String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}";
+        Pattern patternChecker = Pattern.compile(regex);
+        Matcher matchChecker = patternChecker.matcher(userPassword);
+
+        //checking valid or not
+        if(matchChecker.matches()){
+            System.out.println("Valid Password");
+        }
+        else{
+            System.out.println("Invalid Password");
+        }
     }
 }
