caa70323b20e72e8200a9cf294c502b5ce3e2524
tejuypatil <tejuypatil@gmail.com>
2022-08-02 18:39:38 +0530

    Added Program to check valid email or not

--- src/com/brigelabz/userresistration/UserRegistration.java
+++ src/com/brigelabz/userresistration/UserRegistration.java
@@ -8,22 +8,22 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         UserRegistration newName = new UserRegistration();
-        System.out.println("Enter First Name : ");
-        String lastName = scanner.nextLine();
-        newName.checkFirstName(lastName);
+        System.out.println("Enter Email : ");
+        String email = scanner.nextLine();
+        newName.checkEmail(email);
     }
-    public void checkFirstName(String lastname){
-        String regex = "[A-Z]{1}[a-z]{2,}";
+    public void checkEmail(String emailId){
+        String regex ="^[a-z0-9]{3,}+([_+-.][a-z0-9]+)?(@[a-z0-9]+)([.][a-z]{2,4})([.][a-z]{2})?$";
 
         Pattern patternChecker = Pattern.compile(regex);
-        Matcher matchChecker = patternChecker.matcher(lastname);
+        Matcher matchChecker = patternChecker.matcher(emailId);
 
         //checking valid or not
         if(matchChecker.matches()){
-            System.out.println("Last name is valid");
+            System.out.println("Valid Email");
         }
         else{
-            System.out.println("Last Name is Invalid");
+            System.out.println("Invalid Email");
         }
     }
 
