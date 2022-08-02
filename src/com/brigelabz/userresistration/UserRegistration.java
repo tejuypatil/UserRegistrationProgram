accbab9de4dc1f6b8ff4ef262cf3941fb7000e29
tejuypatil <tejuypatil@gmail.com>
2022-08-02 18:14:23 +0530

    Added Program to check First name start with capital letter and has 3 characters

--- src/com/brigelabz/userresistration/UserRegistration.java
+++ src/com/brigelabz/userresistration/UserRegistration.java
@@ -1,7 +1,30 @@
 package com.brigelabz.userresistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+        UserRegistration newName = new UserRegistration();
+        System.out.println("Enter First Name : ");
+        String firstName = scanner.nextLine();
+        newName.checkFirstName(firstName);
     }
+    public void checkFirstName(String firstname){
+        String regex = "[A-Z]{1}[a-z]{2,}";
+
+        Pattern patternChecker = Pattern.compile(regex);
+        Matcher matchChecker = patternChecker.matcher(firstname);
+
+        //checking valid or not
+        if(matchChecker.matches()){
+            System.out.println("First name is valid");
+        }
+        else{
+            System.out.println("First Name is Invalid");
+        }
+    }
+
 }
